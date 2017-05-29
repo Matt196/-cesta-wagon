4b47e0979f5bd3c1efe791e536be30dd4489be55
matthieu reboul <matthieu.reboul@gmail.com>
2017-05-29 12:45:52 +0200

    create all tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,61 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170529101336) do
+ActiveRecord::Schema.define(version: 20170529104216) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "user_awards", force: :cascade do |t|
-    t.string   "name"
-    t.string   "year"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table "basket_lines", force: :cascade do |t|
+    t.integer "product_id"
+    t.integer "user_id"
+    t.index ["product_id"], name: "index_basket_lines_on_product_id", using: :btree
+    t.index ["user_id"], name: "index_basket_lines_on_user_id", using: :btree
   end
 
-  create_table "user_reviews", force: :cascade do |t|
-    t.text     "description"
-    t.integer  "mark"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+  create_table "producer_awards", force: :cascade do |t|
+    t.string  "name"
+    t.string  "year"
+    t.integer "producer_id"
+    t.index ["producer_id"], name: "index_producer_awards_on_producer_id", using: :btree
+  end
+
+  create_table "producer_reviews", force: :cascade do |t|
+    t.text    "content"
+    t.integer "mark"
+    t.integer "user_id"
+    t.integer "producer_id"
+    t.index ["producer_id"], name: "index_producer_reviews_on_producer_id", using: :btree
+    t.index ["user_id"], name: "index_producer_reviews_on_user_id", using: :btree
+  end
+
+  create_table "producers", force: :cascade do |t|
+    t.string  "name"
+    t.string  "address"
+    t.string  "zipcode"
+    t.string  "city"
+    t.text    "description"
+    t.string  "phone"
+    t.string  "mobile_phone"
+    t.string  "company_email"
+    t.string  "category"
+    t.integer "user_id",       null: false
+    t.index ["user_id"], name: "index_producers_on_user_id", using: :btree
+  end
+
+  create_table "product_awards", force: :cascade do |t|
+    t.string  "name"
+    t.string  "year"
+    t.integer "product_id"
+    t.index ["product_id"], name: "index_product_awards_on_product_id", using: :btree
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string  "name"
+    t.string  "description"
+    t.integer "price"
+    t.integer "producer_id"
+    t.index ["producer_id"], name: "index_products_on_producer_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -48,4 +86,12 @@ ActiveRecord::Schema.define(version: 20170529101336) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "basket_lines", "products"
+  add_foreign_key "basket_lines", "users"
+  add_foreign_key "producer_awards", "producers"
+  add_foreign_key "producer_reviews", "producers"
+  add_foreign_key "producer_reviews", "users"
+  add_foreign_key "producers", "users"
+  add_foreign_key "product_awards", "products"
+  add_foreign_key "products", "producers"
 end
