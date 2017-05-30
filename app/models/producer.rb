228e42e0736c15f18d2f29df7a862c87fe6d52a7
guillaume crouillere <gcrouillere@hotmail.fr>
2017-05-30 11:20:38 +0200

    user coordinate sent to URL

--- app/models/producer.rb
+++ app/models/producer.rb
@@ -1,16 +1,22 @@
 class Producer < ApplicationRecord
-belongs_to :user
-has_many :products, dependent: :destroy
-has_many :producer_awards, dependent: :destroy
+  #Tables relations
+  belongs_to :user
+  has_many :products, dependent: :destroy
+  has_many :producer_awards, dependent: :destroy
 
-validates :name, presence: true, uniqueness: true
-validates :address, presence: true
-validates :zipcode, presence: true
-validates :city, presence: true
-validates :description, presence: true
-validates :phone, presence: true, unless: ->(producer){producer.mobile_phone.present?}
-validates :mobile_phone, presence: true, unless: ->(producer){producer.phone.present?}
-validates :company_email, presence: true, uniqueness: true
-validates :category, presence: true
-validates :user, presence: true
+  #Geocoder
+  geocoded_by :address
+  after_validation :geocode
+
+  #Rows validations
+  validates :name, presence: true, uniqueness: true
+  validates :address, presence: true
+  validates :zipcode, presence: true
+  validates :city, presence: true
+  validates :description, presence: true
+  validates :phone, presence: true, unless: ->(producer){producer.mobile_phone.present?}
+  validates :mobile_phone, presence: true, unless: ->(producer){producer.phone.present?}
+  validates :company_email, presence: true, uniqueness: true
+  validates :category, presence: true
+  validates :user, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170529104216) do
+ActiveRecord::Schema.define(version: 20170529132705) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,8 @@ ActiveRecord::Schema.define(version: 20170529104216) do
     t.string  "company_email"
     t.string  "category"
     t.integer "user_id",       null: false
+    t.float   "latitude"
+    t.float   "longitude"
     t.index ["user_id"], name: "index_producers_on_user_id", using: :btree
   end
 
