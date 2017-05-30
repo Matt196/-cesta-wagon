f079b5b236fdf805ee515c19da06926780eb73f4
clement berger <clement.berger@gmail.com>
2017-05-30 16:59:47 +0200

    email fonctionnel en local - parametré pour l'exemple lors de l'ajout d'un producteur

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
+  after_create :send_welcome_email
+
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    binding.pry
+    send_welcome_email(@user)
+  end
+
+  private
+    def send_welcome_email(user)
+      binding.pry
+      UserMailer.welcome_email(user).deliver_now
+    end
+
+
 end
+
+
+
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,6 +4,6 @@ default from: 'notifications@example.com'
   def welcome_email(user)
     @user = user
     @url  = 'http://example.com/login'
-    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+    mail(to: user, subject: 'Welcome to My Awesome Site')
   end
 end
