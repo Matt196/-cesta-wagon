c9323786ee4c15eb8ef688b4a137f063888b025a
clement berger <clement.berger@gmail.com>
2017-06-02 20:33:20 +0200

    basket premiere version (juste un degrossissment)

--- app/controllers/basket_lines_controller.rb
+++ app/controllers/basket_lines_controller.rb
@@ -3,19 +3,21 @@ class BasketLinesController < ApplicationController
   # before_action :id_producer_review, only: [:show, :edit, :update, :destroy]
   # before_action :id_producer, only: [:new, :create]
   # skip_before_action :authenticate_user!, only: [:index, :show]
-  skip_after_action :verify_authorized, only: [:create, :show]
+  skip_after_action :verify_authorized, only: [:new, :show]
 
 # Il faudra activer pundit dans un second temps
 
-  def show
+  def new
+
+
+    @basket = BasketLine.new(
+      user_id: current_user.id,
+      product_id: params["format"]
+      )
+    @basket.save
   end
 
-  def create
-
-    # en cours de contruction par clément, ce n'est pas fini. cela ne marche donc pas encore. Merci
-    @basket.create(
-      user: current_user
-      product: Product.find(params[:product_id])
-    )
+  def show
+    @basket = BasketLine.where(user_id: current_user.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   end
 
-  resource :basket_lines, only: [:show, :create]
+  resource :basket_lines, only: [:show, :new, :create]
 
   resources :products, only:[:index, :show, :edit, :update, :destroy]
 
