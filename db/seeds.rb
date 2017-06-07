489d0a9cdbfbfcac332411efc082cc3b3cb6029c
guillaume crouillere <gcrouillere@hotmail.fr>
2017-06-07 17:39:39 +0200

    seed and basket css

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,63 +1,71 @@
 
 ProducerAward.destroy_all
-ProducerReview.destroy_all
+# ProducerReview.destroy_all
 Product.destroy_all
-Producer.destroy_all
-User.destroy_all
+# Producer.destroy_all
+# User.destroy_all
 
 
-# --------------SCRAPER PRODUCER without PHOTOS--------------------
+# # --------------SCRAPER PRODUCER without PHOTOS--------------------
 
-year = 2016
-scrapper = Scrapper::ConcoursAgricoleScrapper.new(year: year.to_s)
-scrapper.scrap.each do |data|
+# year = 2016
+# scrapper = Scrapper::ConcoursAgricoleScrapper.new(year: year.to_s)
+# scrapper.scrap.each do |data|
 
-  user = User.create(
-    email: data[:company_email],
-    first_name: data[:name],
-    last_name: data[:zipcode],
-    password: "password"
-  )
+#   user = User.create(
+#     email: data[:company_email],
+#     first_name: data[:name],
+#     last_name: data[:zipcode],
+#     password: "password"
+#   )
 
 
-  producer = Producer.create(
-    name: data[:name],
-    address: data[:address],
-    zipcode: data[:zipcode],
-    city: data[:city],
-    description: data[:description],
-    phone: data[:phone],
-    company_email: data[:company_email],
-    category: data[:category],
-    user: user
-  )
+#   producer = Producer.create(
+#     name: data[:name],
+#     address: data[:address],
+#     zipcode: data[:zipcode],
+#     city: data[:city],
+#     description: data[:description],
+#     phone: data[:phone],
+#     company_email: data[:company_email],
+#     category: data[:category],
+#     user: user
+#   )
 
-  ProducerAward.create(
-    producer: producer,
-    name: data[:medaille],
-    year: year
-  )
+#   ProducerAward.create(
+#     producer: producer,
+#     name: data[:medaille],
+#     year: year
+#   )
 
-  # sleep(3)
-end
+#   # sleep(3)
+# end
 
 # --------------ADD 3 products to PRODUCER without PHOTOS--------------------
 # url = "http://lorempixel.com/800/600/city/"
 
 
-Producer.all.each do |elem|
-  3.times do
+Producer.all.each do |producer|
+  rand(1..4).times do
+
+    if AUTHORIZED_CATEGORIES[producer.category].present?
+      name = AUTHORIZED_CATEGORIES[producer.category][:keywords].sample
+    else
+       name = Faker::Commerce.product_name
+    end
     product = Product.create(
-      name: Faker::Commerce.product_name,
+      name: name,
       description: Faker::Lorem.paragraph,
       price: Faker::Commerce.price,
-      producer: elem
+      producer: producer
       )
   end
 end
 
 Product.all.each do |product|
-  ProductAward.new(name: ["gold", "silver", "bronze"].sample, year:"2017", product: product)
+  puts product.name
+  ProductAward.create(name: ["gold", "silver", "bronze", "n"].sample, year:"2017", product: product)
+  puts product.product_awards.first.name
 end
 
 # --------------ADD main photo to PRODUCER --------------------
