18b66d05087968dff9beb0895f20f979079d8bb3
clement berger <clement.berger@gmail.com>
2017-06-08 12:18:14 +0200

    flash notice + faviscon + meta

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -57,5 +57,9 @@ class ApplicationController < ActionController::Base
     @basket = Basket_line.new
   end
 
+  def default_url_options
+    { host: ENV["HOST"] || "localhost:3000" }
+  end
+
 end
 
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,8 @@
 class Product < ApplicationRecord
   belongs_to :producer
   has_many :product_awards, dependent: :destroy
-  # has_many :producer_reviews, dependent: :destroy
+  has_many :basket_lines, dependent: :destroy
+
 
   has_attachments :photos, maximum: 3
 
