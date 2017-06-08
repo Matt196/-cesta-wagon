7a0c782aafd3fa7a5390a56ccb3fcc090631394d
matthieu reboul <matthieu.reboul@gmail.com>
2017-06-08 14:18:32 +0200

    mobile filter features with scroll

--- app/assets/javascripts/filters.js
+++ app/assets/javascripts/filters.js
@@ -1,8 +1,11 @@
 $(function() {
   $('#mobile-filter-show').on('click', function() {
     $('#mobile-filter-container').css({'top':0});
+    $('body').addClass('filter-opened');
   });
+
   $('#mobile-filter-hide').on('click', function() {
     $('#mobile-filter-container').css({'top': '100vh'});
+    $('body').removeClass('filter-opened');
   });
 });
