889ad06be8d337762473cb29b94b38f956b8f829
matthieu reboul <matthieu.reboul@gmail.com>
2017-06-08 22:34:39 +0200

    seed 2015

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@
 
 # --------------SCRAPER PRODUCER without PHOTOS--------------------
 
-year = 2016
+year = 2015
 scrapper = Scrapper::ConcoursAgricoleScrapper.new(year: year.to_s)
 scrapper.scrap.each do |data|
 
@@ -78,21 +78,21 @@ end
 
 # --------------ADD 3 random rewiew to PRODUCER ----------------
 nice_user = User.create(
-  email: "nice_user1@cesta.top",
+  email: "nice_user2015@cesta.top",
   first_name: 'Brice',
   last_name: 'De Nice',
   password: "password"
 )
 
 avg_user = User.create(
-  email: "avg_user1@cesta.top",
+  email: "avg_user2015@cesta.top",
   first_name: 'Jean',
   last_name: 'Dupont',
   password: "password"
 )
 
 bad_user = User.create(
-  email: "bad_user1@cesta.top",
+  email: "bad_user2015@cesta.top",
   first_name: 'Docteur',
   last_name: 'House',
   password: "password"
